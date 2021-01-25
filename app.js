a5baa1125108e07f1e0e4b007829e65826e95afc
Linkin Ben <linkinben@localhost.localdomain>
2021-01-25 18:25:58 +0700

    Update Minor Directories and App.js Connection String Stuff

--- app.js
+++ app.js
@@ -1,13 +1,59 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const passport = require('passport');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const app = express();
 
-var app = express();
+dotenv.config();
+
+app.use(
+  session({
+    secret: process.env.secretOrKey,
+    reSave: true,
+    saveUninitialized: true,
+    cookie: { secure: false },
+  }),
+);
+
+// For later research
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.enable('trust proxy');
+app.use(
+  cors({
+    credentials: true,
+  }),
+);
+
+// For socket if use
+const http = require('http');
+
+const server = http.createServer(app);
+
+// Mongo configuration
+mongoose.connect(process.env.URI || 'localhost:27017', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => {
+  console.log('Mongo is connecting');
+}).catch((err) => {
+  console.log(err);
+});
+
+mongoose.connection.on('connected', () => {
+  console.log('Mongo is successfully connected');
+});
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -23,12 +69,12 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -38,4 +84,4 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-module.exports = app;
+module.exports = { app, server };
